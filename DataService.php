737b115f24f1f60dbfb9460f92bd91cecea2efbb
Adam <adam561234@gmail.com>
2023-06-10 16:18:14 +0200

    Initial commit

---
+++ DataService.php
@@ -0,0 +1,29 @@
+<?php
+
+class DataService
+{
+    private string $url;
+
+    /**
+     * @param string $url
+     */
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @return bool|string
+     */
+    public function getData()
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $resp = curl_exec($curl);
+        curl_close($curl);
+
+        return $resp;
+    }
+}
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+// todo get rows from database
+// todo make <table> and fill it with rows from database
