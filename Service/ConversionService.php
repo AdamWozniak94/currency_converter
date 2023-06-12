65d47d338f68724b517e663702f89b2832f65404
Adam <adam561234@gmail.com>
2023-06-12 16:22:19 +0200

    remove parameter from ConversionService constructor + other minor changes

--- Service/ConversionService.php
+++ Service/ConversionService.php
@@ -6,15 +6,18 @@ class ConversionService
 {
     private CurrencyRepository $currencyRepository;
 
-    /**
-     * @param CurrencyRepository $currencyRepository
-     */
-    public function __construct(CurrencyRepository $currencyRepository)
+    public function __construct()
     {
-        $this->currencyRepository = $currencyRepository;
+        $this->currencyRepository = new CurrencyRepository;
     }
 
 
+    /**
+     * @param float $amount
+     * @param int $baseCurrencyId
+     * @param int $targetCurrencyId
+     * @return float
+     */
     public function convert(float $amount, int $baseCurrencyId, int $targetCurrencyId): float
     {
         // 0 is PLN
